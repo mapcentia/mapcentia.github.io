b064e17552c9cdf2e1b94a60aa5d0880a9632bb0
Martin Høgh <mh@mapcentia.com>
2014-12-12 10:46:51 +0100

    trackunit v3

---
+++ trackunit_v3/build/temp/production/MyApp/sass/config.rb
@@ -0,0 +1,3 @@
+require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
+require '../../../../../sass/config.rb'
+cache_path = '/Users/mh/Documents/www/mapcentia.github.io/trackunit_v3/build/.sass-cache'
---
+++ trackunit_v3/build/temp/production/MyApp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/mh/Documents/www/mapcentia.github.io/trackunit_v3/app.js
