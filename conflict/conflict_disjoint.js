73bc946fc17bea41825a16774803721e1c9ebd24
Martin Høgh <mh@mapcentia.com>
2015-11-10 10:39:36 +0100

    Layer tree

--- conflict/conflict_disjoint.js
+++ conflict/conflict_disjoint.js
@@ -95,7 +95,7 @@ var cowi = (function () {
                 map = {};
                 $.ajax({
                     url: 'http://eu1.mapcentia.com/api/v1/elasticsearch/search/dk/aws4/' + type,
-                    data: 'call_counter=' + (++call_counter) + '&q={"query":{"filtered":{"query":{"query_string":{"default_field":"string","query":"' + encodeURIComponent(query.toLowerCase().replace(",", "")) + '","default_operator":"AND"}},"filter":{"term":{"municipalitycode":"0' + komKode + '"}}}}}',
+                    data: 'call_counter=' + (++call_counter) + '&q={"query":{"filtered":{"query":{"query_string":{"default_field":"string","query":"' + encodeURIComponent(query.toLowerCase().replace(",", "")) + '","default_operator":"AND"}},"filter":{"term":{"kommunekode":"0' + komKode + '"}}}}}',
                     contentType: "application/json; charset=utf-8",
                     scriptCharset: "utf-8",
                     dataType: 'jsonp',
