56251c1f94c71bc71731d8624a80d7382fd8df35
MapCentia <mh@mapcentia.com>
2016-09-30 13:55:42 +0200

    Update lpsearch.js

--- lpsearch/lpsearch.js
+++ lpsearch/lpsearch.js
@@ -11,7 +11,7 @@ var cowiLpSearch = (function () {
                 var search = _.debounce(function (query, process) {
                     map = {};
                     $.ajax({
-                        url: 'http://cowi.mapcentia.com/api/v1/elasticsearch/search/dk/planer/lokalplan',
+                        url: 'http://cowi.mapcentia.com/api/v1/elasticsearch/search/dk/planer/lokalplaner',
                         data: '&size=20&q={"query":{"filtered":{"query":{"query_string":{"default_field":"string","query":"' + encodeURIComponent(query.toLowerCase()) + '","default_operator":"AND"}},"filter":{"term":{"properties.komnr":"' + conf.komnr + '"}}}}}',
                         dataType: 'jsonp',
                         contentType: "application/json; charset=utf-8",
